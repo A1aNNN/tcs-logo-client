871fab456c29340979f247277f1a88e05695948f
Alan Wang <alanwang123919@gmail.com>
2024-01-01 14:06:14 -0800

    added logo

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import "./../App.css";
+// import logo from "./images/TCSLogo.svg"
+import logo from './../images/TCSLogo.png'
+import { Box, Image } from '@chakra-ui/react';
 
 const Start = () => {
   return (
-    <div>
-      hi
-    </div>
+    <Box className='app'>
+        <Image className='logo' src={logo}/>
+    </Box>
   )
 }
 
