05d22bac3e512882d5c0468d685f3a106d9510fc
Alan Wang <alanwang123919@gmail.com>
2024-01-01 13:39:51 -0800

    added chakra ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import axios from 'axios';
 import { useState } from "react";
+import Start from "./components/Start";
 
 function App() {
 
@@ -11,11 +12,15 @@ function App() {
     e.preventDefault();
 
     axios
+
+      // Send a post request to specific port and url
       .post("http://localhost:8080/generate-text", { prompt })
+      
       .then((res) => {
         console.log('Axios Response:', res);
         setResponse(res?.data?.content);
       })
+      
       .catch((err) => {
         console.error('Axios Error:', err);
       });
@@ -23,61 +28,34 @@ function App() {
 
   return (
     <>
-    <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="inline-full-name">
-              Just say/ask anything :
-            </label>
-          </div>
-          <div>
-            <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
-          </div>
-          <div>
-            <button type="submit">
-              Submit
-            </button>
-          </div>
-        </form>
-        <div>
-          <p>{response}</p>
-        </div>
-
-      </div>
-    </div>
+      <Start/>
     </>
+    // <>
+    // <div>
+    //   <div>
+    //     <form onSubmit={handleSubmit}>
+    //       <div>
+    //         <label htmlFor="inline-full-name">
+    //           Just say/ask anything :
+    //         </label>
+    //       </div>
+    //       <div>
+    //         <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
+    //       </div>
+    //       <div>
+    //         <button type="submit">
+    //           Submit
+    //         </button>
+    //       </div>
+    //     </form>
+    //     <div>
+    //       <p>{response}</p>
+    //     </div>
+
+    //   </div>
+    // </div>
+    // </>
   )
 }
 
 export default App;
-
-
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-
-// function App() {
-//   const [generatedText, setGeneratedText] = useState('');
-
-//   useEffect(() => {
-//     fetch('http://localhost:8080/generate-text')
-//       .then((response) => response.text())
-//       .then((text) => {
-//         console.log('Text received from server:', text)
-//         setGeneratedText(text)
-//       })
-//       .catch((error) => console.error('Error fetching generated text:', error));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Generated Text:</h1>
-//       <p>{generatedText}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
