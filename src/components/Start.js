ae33ba835c8ff3c49196bfb3f91c235511e2cac3
Alan Wang <alanwang123919@gmail.com>
2024-01-01 14:33:08 -0800

    general layout of the starting page

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,12 +2,33 @@ import React from 'react'
 import "./../App.css";
 // import logo from "./images/TCSLogo.svg"
 import logo from './../images/TCSLogo.png'
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Button, Image, Text } from '@chakra-ui/react';
 
 const Start = () => {
   return (
-    <Box className='app'>
-        <Image className='logo' src={logo}/>
+    <Box className='app' minH='100vh' w='100%'>
+        <Box className='logo-box'>
+            <Image className='logo' src={logo}/>
+        </Box>
+
+        <Box className='content' display='flex' flexDirection='column'>
+            <Text className='text-title'>
+                Logo Generator
+            </Text>
+            <Text className='start-paragraph'>
+                Welcome to the TCS Logo Generator! Please attach a PNG image of 
+                your current logo, as well as a few points of improvement to receive 
+                three improved logos.
+            </Text>
+            <Text className='start-paragraph'>
+                Please keep in mind that these logos are tentative. To receive a 
+                well flushed out product, please fill out the Typeform to meet 
+                with the TCS team!
+            </Text>
+            <Button className='start-button'>
+                Get started here
+            </Button>
+        </Box>
     </Box>
   )
 }
