6313e734870fcfb9da9fc1569b2f86b244e6cc47
Alan Wang <alanwang123919@gmail.com>
2023-12-31 20:34:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from 'axios';
+import { useState } from "react";
 
 function App() {
+
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8080/generate-text", { prompt })
+      .then((res) => {
+        console.log('Axios Response:', res);
+        setResponse(res?.data?.content);
+      })
+      .catch((err) => {
+        console.error('Axios Error:', err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="inline-full-name">
+              Just say/ask anything :
+            </label>
+          </div>
+          <div>
+            <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
+          </div>
+          <div>
+            <button type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+        <div>
+          <p>{response}</p>
+        </div>
+
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+
+// function App() {
+//   const [generatedText, setGeneratedText] = useState('');
+
+//   useEffect(() => {
+//     fetch('http://localhost:8080/generate-text')
+//       .then((response) => response.text())
+//       .then((text) => {
+//         console.log('Text received from server:', text)
+//         setGeneratedText(text)
+//       })
+//       .catch((error) => console.error('Error fetching generated text:', error));
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>Generated Text:</h1>
+//       <p>{generatedText}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
