07d11dacc3e7e46071b8c3f5dc6109755a530162
Alan Wang <alanwang123919@gmail.com>
2024-01-09 18:25:00 -0800

    finished page for good outcome (i.e. user has clicked yes)

--- src/components/GoodOutcome.js
+++ src/components/GoodOutcome.js
@@ -1,12 +1,29 @@
+import { Box, Button, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import logo from './../images/TCSLogo.png'
+import Footer from './Footer'
 
-const GoodOutcome = () => {
+const GoodOutcome = ({ redo5 }) => {
   return (
-    <div>
-      hi
-    </div>
+    <Box className='app' minH='100vh' w='100%'>
+        <Box className='logo-box'>
+            <Image className='logo' src={logo}/>
+        </Box>
+        <Box className='content' display='flex' flexDirection='column'>
+            <Text className='text-title'>
+                Logo Generator
+            </Text>
+
+            <Text className='text-paragraph'>
+                This was just a sneak peak of what we're capable of! To get a well-polished final product, fill out the Typeform to meet with the TCS team!
+            </Text>
+
+            <Button className='restart-button' onClick={redo5}>Restart</Button>
+        </Box>
+
+        <Footer/>
+    </Box>
   )
 }
 
 export default GoodOutcome
-hi
