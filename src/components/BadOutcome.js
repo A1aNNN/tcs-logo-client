37331725af424781426592f2261bb6ac44e47a8f
Alan Wang <alanwang123919@gmail.com>
2024-01-09 17:55:54 -0800

    created 2 new pages, where one appears depending on if the user liked the logo or not

---
+++ src/components/BadOutcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BadOutcome = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default BadOutcome
+hi
---
+++ src/components/GoodOutcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GoodOutcome = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default GoodOutcome
+hi
