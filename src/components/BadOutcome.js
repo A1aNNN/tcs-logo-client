5465a7b18763f30dc7ad0aae20d93ec384ae7cc5
Alan Wang <alanwang123919@gmail.com>
2024-01-09 18:25:22 -0800

    finished page for bad outcome (i.e. user has clicked no)

--- src/components/BadOutcome.js
+++ src/components/BadOutcome.js
@@ -1,12 +1,29 @@
+import { Box, Button, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import logo from './../images/TCSLogo.png'
+import Footer from './Footer'
 
-const BadOutcome = () => {
+const BadOutcome = ({ redo6 }) => {
   return (
-    <div>
-      hi
-    </div>
+    <Box className='app' minH='100vh' w='100%'>
+        <Box className='logo-box'>
+            <Image className='logo' src={logo}/>
+        </Box>
+        <Box className='content' display='flex' flexDirection='column'>
+            <Text className='text-title'>
+                Logo Generator
+            </Text>
+
+            <Text className='text-paragraph'>
+                Well, we don't like AI generated images either! Fill out the Typeform to schedule a call with the TCS team!
+            </Text>
+
+            <Button className='restart-button' onClick={redo6}>Restart</Button>
+        </Box>
+
+        <Footer/>
+    </Box>
   )
 }
 
 export default BadOutcome
-hi
