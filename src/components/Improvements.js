ff20880d02d6d78250401cfb2fd576992aa77755
Alan Wang <alanwang123919@gmail.com>
2024-02-08 02:33:44 -0800

    finished second page for user inputting name of business

--- src/components/Improvements.js
+++ src/components/Improvements.js
@@ -2,6 +2,7 @@ import { Box, Image, Input, Text } from '@chakra-ui/react'
 import logo from './../images/TCSLogo.png'
 import React, { useEffect, useState } from 'react'
 import Footer from './Footer';
+import Logo from './Logo';
 
 const Improvements = ({ onNext, handleSubmit, updateUserPrompt, parseImage }) => {
 
@@ -33,11 +34,28 @@ const Improvements = ({ onNext, handleSubmit, updateUserPrompt, parseImage }) =>
 
   return (
     <Box className='app' minH='100vh' w='100%'>
-        <Box className='logo-box'>
+        {/* <Box className='logo-box'>
             <Image className='logo' src={logo}/>
+        </Box> */}
+        <Logo/>
+
+        <Box className='content prompt-container' display='flex' flexDirection='column'>
+            <Text className='text-step text-title-upload'>
+                Step 2
+            </Text>
+            <Text className='text-instruction-black'>
+                Tell us the <span style={{ color: '#969696'}}> name</span> of your business.
+            </Text>
+            <Input
+                className='improvements-input'
+                placeholder='-> Type your answer here...'
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleKeyPress}
+            />
         </Box>
 
-        <Box className='content' display='flex' flexDirection='column'>
+        {/* <Box className='content' display='flex' flexDirection='column'>
             <Text className='text-title'>
                 Logo Generator
             </Text>
@@ -51,9 +69,9 @@ const Improvements = ({ onNext, handleSubmit, updateUserPrompt, parseImage }) =>
                 onChange={(e) => setInputValue(e.target.value)}
                 onKeyDown={handleKeyPress}
             />
-        </Box>
+        </Box> */}
 
-        <Footer/>
+        {/* <Footer/> */}
     </Box>
   )
 }
